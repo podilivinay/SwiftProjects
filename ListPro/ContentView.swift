0c1f58c770fd182ae4bf52aee55279a51650e89f
Vinay Kumar <vinay_podili@brighterbee.com>
2020-03-02 15:51:04 +0530

    Initial Commit

---
+++ ListPro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListPro
+//
+//  Created by Vinay Podili on 3/2/20.
+//  Copyright © 2020 Vinay Podili. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
